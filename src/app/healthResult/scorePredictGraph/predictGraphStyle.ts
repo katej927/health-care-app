e18c5f304067a99b64e1363256dfd05426aa8fcf
rlaebqebq <rlaebqebq@gmail.com>
2022-05-28 12:03:59 +0900

    feat : 애니메이션, fontfamily, fontweight 추가

--- src/app/healthResult/scorePredictGraph/predictGraphStyle.ts
+++ src/app/healthResult/scorePredictGraph/predictGraphStyle.ts
@@ -1,15 +1,23 @@
-import { COLORS, SIZES } from '../_shared';
+import { COLORS, SIZES, FONT_WEIGHT, FONT_FAMILY } from '../_shared';
 
 const PredictGraphStyle = {
   chart: {
     domainPadding: 20,
     height: 250,
   },
+  animate: {
+    duration: 1000,
+    onLoad: { duration: 2000 },
+  },
 
   bar: {
     barWidth: 35,
     barRatio: 1,
     cornerRadius: 0,
+    animate: {
+      duration: 500,
+      onLoad: { duration: 2000 },
+    },
   },
 
   label: {
@@ -20,6 +28,10 @@ const PredictGraphStyle = {
   line: {
     style: {
       data: { stroke: COLORS.$GREY_02 },
+    },
+    animate: {
+      duration: 0,
+      onLoad: { duration: 2000 },
     },
   },
 
@@ -34,7 +46,7 @@ const PredictGraphStyle = {
       ticks: {
         stroke: 'transparent',
       },
-      tickLabels: { fontSize: SIZES.$REGULAR },
+      tickLabels: { fontFamily: FONT_FAMILY, fontSize: SIZES.$REGULAR, fontWeight: FONT_WEIGHT.$SEMI_BOLD },
     },
   },
 };
